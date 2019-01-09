7516a21340607e1b70a9d9c2364b1616a409abea
Tyler Glen <tylerbumguardner@gmail.com>
2019-01-09 14:20:07 -0700

    updated columns

--- src/components/GameCard/index.js
+++ src/components/GameCard/index.js
@@ -4,7 +4,7 @@ import "./GameCard.css";
 function GameCard(props) {
     return (
         <div 
-        className="allCards" 
+        className="allCards col-xs-6 col-sm-6 col-md-4 col-lg-3 col-xl-3" 
         onClick={() => {props.handleImageClick(props.id, props.clicked)}}>
             <img src={props.image} id={props.id} alt="alt"/>
         </div>
